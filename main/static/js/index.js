e911c0c59bbe2307b37c83393a6c93522da78181
samiroutka <sarkisovmichail007@gmail.com>
2023-04-05 18:41:47 +0300

    commit 6

--- main/static/js/index.js
+++ main/static/js/index.js
@@ -1,13 +1,10 @@
 
-// window.addEventListener('load', () => {
-  // document.querySelector('body').style.display = 'block'    
-// })
-
+// Предзагрузка
 $(document).ready(() => {
   setTimeout(() => {
     document.querySelector('.preloading').style.display = 'none'
     document.querySelector('.wrapper').style.display = 'block'
-  }, 2000);
+  }, 100);
 })
 
 // Создание полосок сверху
@@ -19,9 +16,10 @@ for (let roomStripe of roomStripes){
   roomStripe.firstChild.classList.add('selected')
 }
 
-// работа слайдера
+// работа слайдера (типо insta)
 let transitionDuration = getComputedStyle(document.querySelector('.room').children[0])['transitionDuration']
 let testOfDuration = false // проверка на завершение анимации room
+let moveTest = true
 document.addEventListener('click', (event) => {
   if (testOfDuration && event.target.closest('.enlarge') && event.target.closest('.roomImages')){
     let currentTransform = parseInt(event.target.closest('.roomImages').style.transform.slice(11, -1)) ?
@@ -48,6 +46,31 @@ document.addEventListener('click', (event) => {
     event.target.closest('.roomSlider').querySelector('.roomStripes').children[currentImage].classList.add('selected')
   }
 })
+// перетаскивания номеров с помощью мыши (.rooms)
+let roomsElement = document.querySelector('.rooms')
+let startCoordinateX = 0
+let scrollTest = false
+let roomsElementCurrentScroll = 0
+roomsElement.addEventListener('mousedown', (event) => {
+  if (!event.target.closest('.room')){
+    scrollTest = true
+    startCoordinateX = event.clientX
+  }
+})
+roomsElement.addEventListener('mousemove', (event) => {
+  if (scrollTest){
+    roomsElement.scrollLeft = roomsElementCurrentScroll + (startCoordinateX - event.clientX)
+    moveTest = false
+  }
+})
+roomsElement.addEventListener('mouseup', (event) => {
+  scrollTest = false
+  roomsElementCurrentScroll = roomsElement.scrollLeft
+  setTimeout(() => {
+    moveTest = true
+  }, 10);
+})
+
 
 
 function resetRoom(node) {
@@ -68,7 +91,7 @@ function enlargeRoom(node){
 // Открытие room
 document.addEventListener('click', (event) => {
   let currentRoom = event.target.closest('.room')
-  if (currentRoom){
+  if (currentRoom && moveTest){
     if (!document.querySelector('.enlarge')){
       enlargeRoom(currentRoom)
     } else if (document.querySelector('.enlarge') && !currentRoom.classList.contains('enlarge')) {
@@ -82,7 +105,44 @@ document.addEventListener('click', (event) => {
   }
 })
 
-$('.slider').slick({
-  infinite: false,
-  dots: true,
+// Календарь (main .booking)
+let calendar = new AirDatepicker('.calendar', {
+  minDate: new Date('2023-03-27'),
+  isMobile: true,
+  autoClose: true,
+  autoClose: false,
+  range: true,
+  multipleDatesSeparator: ' - ',
+  onRenderCell({date, cellType}){
+    if (cellType == 'day') { // проверяем тип ячейки
+      if ( ((date.getMonth()+1) == 7) && ([3,7,8,9].indexOf(date.getDate()) != -1) ){
+        return {disabled:true}
+      }
+    }
+  }
 })
+
+let calendarInput = document.querySelector('.calendar')
+let calendarSubmit = document.querySelector('.booking__submit')
+calendarSubmit.addEventListener('click', () => {
+  let dataOfRoom = document.querySelector('.booking__room select').value
+  let dataOfDates = document.querySelector('.booking__dates input').value
+  let dataOfPeople = document.querySelector('.booking__people select').value
+  let dataOfPhone = document.querySelector('.booking__phone input').value
+  if (dataOfRoom && dataOfDates && dataOfPeople && dataOfPhone){
+    console.log(true)
+  } else{
+    console.log(false)
+  }
+})
+
+// При resize окна
+document.addEventListener('resize', () => {
+  let enlargeRoomElement = document.querySelector('.enlarge')
+  if (enlargeRoomElement) {
+    resetRoom(enlargeRoomElement)
+    setTimeout(() => {
+      enlargeRoomElement.scrollIntoView({behavior: "smooth", block: "center", inline: "center"})
+    }, parseFloat(transitionDuration)*1000);  }
+})
+

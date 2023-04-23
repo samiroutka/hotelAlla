3a7bdefa44d367fd62f3e836e61e38636d5f0b81
samiroutka <sarkisovmichail007@gmail.com>
2023-04-23 18:15:08 +0300

    commit 10

--- main/static/js/index.js
+++ main/static/js/index.js
@@ -10,14 +10,17 @@ document.addEventListener('DOMContentLoaded', () => {
   }, 1000);
 })
 
-// Создание полосок сверху----------------------------------------
-let roomStripes = document.querySelectorAll('.roomStripes')
-for (let roomStripe of roomStripes){
-  for (let element of roomStripe.parentElement.querySelectorAll('img')){
-    element.parentElement.parentElement.querySelector('.roomStripes').append(document.createElement('div'))
-  }
-  roomStripe.firstChild.classList.add('selected')
-}
+// Swiper
+new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 // Очищение полей формы
 let bookingBtn = document.querySelector('.booking__submit')
@@ -29,93 +32,6 @@ function clearFields() {
   document.querySelector('.booking__phone input').value = ''
 }
 clearFields()
-
-// работа слайдера (типо insta)----------------------------------------
-let transitionDuration = getComputedStyle(document.querySelector('.room').children[0])['transitionDuration']
-let testOfDuration = false // проверка на завершение анимации room
-let moveTest = true
-document.addEventListener('click', (event) => {
-  if (testOfDuration && event.target.closest('.enlarge') && event.target.closest('.roomImages')){
-    let currentTransform = parseInt(event.target.closest('.roomImages').style.transform.slice(11, -1)) ?
-    parseInt(event.target.closest('.roomImages').style.transform.slice(11, -1)) : 0 
-    // лево
-    if (event.clientX-Math.round(event.target.closest('.roomSlider').getBoundingClientRect()['x'])
-    < event.target.closest('.roomSlider').offsetWidth/2){
-      if (currentTransform != 0){
-        currentTransform = currentTransform+event.target.closest('.roomImage').offsetWidth
-        event.target.closest('.roomImages').style.transform = `translateX(${currentTransform}px)`
-      }
-    // право
-    } else {
-      if (currentTransform != -(event.target.closest('.roomImage').offsetWidth)*((event.target.closest('.roomImages').children).length-1)){
-        currentTransform = currentTransform-event.target.closest('.roomImage').offsetWidth
-        event.target.closest('.roomImages').style.transform = `translateX(${currentTransform}px)`
-      }
-    }
-    // обработка полос сверху
-    let currentImage = (-currentTransform / event.target.closest('.roomImage').offsetWidth)
-    for (let element of event.target.closest('.roomSlider').querySelector('.roomStripes').children){
-      element.classList.remove('selected')
-    }
-    event.target.closest('.roomSlider').querySelector('.roomStripes').children[currentImage].classList.add('selected')
-  }
-})
-// перетаскивания номеров с помощью мыши (.rooms)-----------
-let roomsElement = document.querySelector('.rooms')
-let startCoordinateX = 0
-let scrollTest = false
-let roomsElementCurrentScroll = 0
-roomsElement.addEventListener('mousedown', (event) => {
-  if (!event.target.closest('.room')){
-    scrollTest = true
-    startCoordinateX = event.clientX
-  }
-})
-roomsElement.addEventListener('mousemove', (event) => {
-  if (scrollTest){
-    roomsElement.scrollLeft = roomsElementCurrentScroll + (startCoordinateX - event.clientX)
-    moveTest = false
-  }
-})
-roomsElement.addEventListener('mouseup', (event) => {
-  scrollTest = false
-  roomsElementCurrentScroll = roomsElement.scrollLeft
-  setTimeout(() => {
-    moveTest = true
-  }, 10);
-})
-
-function resetRoom(node) {
-  node.querySelector('.roomImages').style.transform = 'translateX(0)'
-  for (let element of node.querySelector('.roomStripes').children){
-    element.classList.remove('selected')
-  }
-  node.querySelector('.roomStripes').firstChild.classList.add('selected')
-}
-function enlargeRoom(node){
-  testOfDuration = false
-  node.classList.add('enlarge')
-  setTimeout(() => {
-    testOfDuration = true
-    node.scrollIntoView({behavior: "smooth", block: "center", inline: "center"})
-  }, parseFloat(transitionDuration)*1000);
-}
-// Открытие room----------------------------------------
-document.addEventListener('click', (event) => {
-  let currentRoom = event.target.closest('.room')
-  if (currentRoom && moveTest){
-    if (!document.querySelector('.enlarge')){
-      enlargeRoom(currentRoom)
-    } else if (document.querySelector('.enlarge') && !currentRoom.classList.contains('enlarge')) {
-      resetRoom(document.querySelector('.enlarge'))
-      document.querySelector('.enlarge').classList.remove('enlarge')
-      enlargeRoom(currentRoom)
-    } else if (event.target.closest('.enlarge') && !event.target.closest('.roomSlider')){
-      currentRoom.classList.remove('enlarge')
-      resetRoom(currentRoom)
-    }
-  }
-})
 
 // Календарь (main .booking)------------------------------------
 async function mainCalendar(){
@@ -185,17 +101,6 @@ async function mainCalendar(){
   })
 }
 mainCalendar()
-
-// При resize окна-------------------------------------------
-document.addEventListener('resize', () => {
-  let enlargeRoomElement = document.querySelector('.enlarge')
-  if (enlargeRoomElement) {
-    resetRoom(enlargeRoomElement)
-    setTimeout(() => {
-      enlargeRoomElement.scrollIntoView({behavior: "smooth", block: "center", inline: "center"})
-    }, parseFloat(transitionDuration)*1000);  
-  }
-})
 
 // Запрашивание звонка----------------------------------------
 bookingBtn.addEventListener('click', () => {
--- manager/views.py
+++ manager/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.base import View
 from django.http import HttpResponse
 import json
+from datetime import date as d
 # ---
 from .models import *
 
@@ -17,7 +18,26 @@ class managerView(View):
       return HttpResponse('OK')
     elif (request.headers.get('SELECTED-DATES')):
       try:
-        return HttpResponse(json.dumps(dates_of_rooms.objects.first().rooms))
+        # Проверка на уже прошедшие даты
+        dates_from_base = json.loads(json.dumps(dates_of_rooms.objects.first().rooms))
+        # print(f'input: {dates_from_base}')
+        final_dates = {}
+        for element in dates_from_base:
+          final_dates[str(element)] = ''
+          dates = dates_from_base[element].split(',')
+          if (dates[0] != ''):
+            current_final_dates = []
+            for date in dates:
+              numbers_of_date = date.split('-')
+              if( d(int(numbers_of_date[0]), int(numbers_of_date[1]), int(numbers_of_date[2])) >= d.today()):
+                current_final_dates.append(date)
+            final_dates[str(element)] = ','.join(current_final_dates)
+        # print(f'output: {final_dates}')
+        # Сохранение отфильтрованных дат
+        current_dates_from_base = dates_of_rooms.objects.first()
+        current_dates_from_base.rooms = final_dates
+        current_dates_from_base.save()
+        return HttpResponse(json.dumps(final_dates))
       except:
         return HttpResponse('NO DATES')
     elif (login):
@@ -30,6 +50,7 @@ class managerView(View):
     
   def post(self, request):
     if (request.headers.get('SAVE-DATES')):
+      # Добавление дат в базу данных
       try:
         date = dates_of_rooms.objects.first()
         date.rooms = json.loads(request.body)
